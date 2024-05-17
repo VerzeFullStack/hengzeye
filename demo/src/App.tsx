6ec98c2782ddadfa43d60016f7722c725c4ed90a
Hengze Ye <1098200471@qq.com>
2024-05-17 15:15:13 -0400

    Added susi, logout, edit profile

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -2,8 +2,68 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { AuthenticatedTemplate, MsalProvider, UnauthenticatedTemplate } from '@azure/msal-react';
+import { IPublicClientApplication, InteractionStatus } from "@azure/msal-browser";
+import { useMsal } from "@azure/msal-react";
+import { loginRequest, b2cPolicies } from './authConfig'; 
 
-function App() {
+type AppProps = {
+  msalInstance: IPublicClientApplication
+};
+
+function LoginComponent () {
+  const { instance, inProgress } = useMsal();
+  const activeAccount = instance.getActiveAccount();
+
+  const handleLoginRedirect = () => {
+    instance
+        .loginPopup({
+            ...loginRequest,
+        })
+        .catch(e => {
+            console.error(e);
+        });
+  };
+
+  const handleLogoutRedirect = () => {
+    instance
+        .logout()
+        .catch(e => {
+            console.error(e);
+        });
+  };
+
+  const handleProfileEdit = () => {
+    if(inProgress === InteractionStatus.None){
+      // @ts-ignore
+      instance.acquireTokenRedirect(b2cPolicies.authorities.editProfile);
+    }
+  };
+
+  return (
+    <>
+      <AuthenticatedTemplate>
+        <div>User logged in: {activeAccount?.idTokenClaims?.name}</div>
+        <button 
+            onClick={handleLogoutRedirect}> 
+                Sign Out 
+        </button>
+        <button  
+            onClick={handleProfileEdit}> 
+                Edit Profile 
+        </button>
+      </AuthenticatedTemplate>
+      <UnauthenticatedTemplate>
+        <button  
+            onClick={handleLoginRedirect}> 
+                Sign In 
+        </button>
+      </UnauthenticatedTemplate>
+    </>
+  )
+};
+
+function App(props: AppProps) {
   const [count, setCount] = useState(0)
 
   const [myBool, setmyBool] = useState(true);
@@ -14,7 +74,8 @@ function App() {
   };
   
   return (
-    <>
+    <MsalProvider instance={props.msalInstance}>
+      <LoginComponent/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -39,8 +100,7 @@ function App() {
       <div>
         {myBool ? <p>Total Count: {count}</p> : <p>False!</p>}
       </div>
-
-    </>
+    </MsalProvider>
   )
 }
 
